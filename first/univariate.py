1cb71abcb82c594eaef30ebe55067b50080f4591
R.H. Stine <bob@waltonstine.net>
2016-03-04 17:31:16 -0500

    Take out some debug prints, and split into 10 bins.

--- first/univariate.py
+++ first/univariate.py
@@ -4,6 +4,7 @@
 
 First program, Data Mgmt & Visualization
 """
+import math
 import pandas
 import numpy
  
@@ -27,12 +28,13 @@ def print_dist(data, index_str):
      
 if __name__ == "__main__":
 
-    #print("Rows/columns in gapminder.csv: ", len(data), "/", len(data.columns))
+    print("Rows/columns in gapminder.csv: ", len(data), "/", len(data.columns))
 
-    #conv_2_num(data, "alcconsumption")
+
     data = pandas.read_csv("gapminder.csv", low_memory=False)
-    data["alcconsumption"] = data["alcconsumption"].convert_objects(convert_numeric = True)
-    bins = numpy.linspace(data.alcconsumption.min(), data.alcconsumption.max(), 5)
+    conv_2_num(data, "alcconsumption")    
+    
+    bins = numpy.linspace(0, data.alcconsumption.max(), 10)
     groups = data.groupby(pandas.cut(data.alcconsumption, bins))
     print("try count by bin:")
     print(groups.count().alcconsumption)
