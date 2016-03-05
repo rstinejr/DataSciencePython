0cf95de56e36c935d04d7a5dfd9f62a5e1d1235d
R.H. Stine <bob@waltonstine.net>
2016-03-05 07:03:21 -0500

    Delete unused functions, tidy up.

--- first/univariate.py
+++ first/univariate.py
@@ -12,31 +12,15 @@ def conv_2_num(data, index_str):
     data[index_str] = data[index_str].convert_objects(convert_numeric = True)
     return
     
-def print_all(data, index_str):
-     print(index_str, ":")
-     rowdat = data[index_str]
-     print(rowdat)
-     print("")
-     return
-
-def print_dist(data, index_str):
-    print(index_str, ":")
-    dist_cnt = data[index_str].value_counts(sort=False)
-    print(dist_cnt)
-    print("")
-    return
-     
 if __name__ == "__main__":
     print("Display univariate distribution of several variables from GapMinder.")
-    
-    data = pandas.read_csv("gapminder.csv", low_memory=False)
-    print("Rows/columns in gapminder.csv: ", len(data), "/", len(data.columns))
-
-    
+    print("")
     print("Frequency distributions by category")
     print("Left-hand column shows range of an item's values.")
     print("Right-hand column is number of observations in that range.")
     
+    data = pandas.read_csv("gapminder.csv", low_memory=False)
+
     conv_2_num(data, "alcconsumption")    
     bins = numpy.linspace(0.0, data["alcconsumption"].max(), 20)
     groups = data.groupby(pandas.cut(data["alcconsumption"], bins))
