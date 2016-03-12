894a9148106db19cbcb04b07ce3fe39c35725af4
R.H. Stine <bob@waltonstine.net>
2016-03-12 08:26:52 -0500

    Investigate printing groups separately, so we can do a better job of formatting the output.

--- first/univariate.py
+++ first/univariate.py
@@ -14,9 +14,19 @@ import numpy
    functions.
 """
 def conv_2_num(data, index_str):
-    data[index_str] = pandas.to_numeric(data[index_str], errors = 'coerce')
+    data[index_str] = pandas.to_numeric(data[index_str], errors = 'coerce').dropna()
     return
 
+"""
+    For a given variable, identified by "index_str", 
+        convert the data to numeric and drop missing data.
+        define 20 equally-sized intervals between the minimum and maximum value
+        return the "groups" -- the data partitioned into these intervals.
+    The return value "groups" is an instance of pandas.core.groupby.DataFrameGroupBy.
+    It is a list of pairs, in which the first item is a string that identifies
+    the "bin" of the group, and the second item is a DataFrame of the
+    selected subset of the original data.
+"""
 def get_groups(data, index_str):
     conv_2_num(data, index_str)    
     bins = numpy.linspace(data[index_str].min(), data[index_str].max(), 20)
@@ -38,11 +48,17 @@ if __name__ == "__main__":
     
     data = pandas.read_csv("gapminder.csv", low_memory=False)
 
-    print("'alcconsumption' is annual per capita consumption of alcohol, in liters.")
+    print("'alcconsumption' is annual per capita consumption of pure alcohol, in liters.")
     groups = get_groups(data, "alcconsumption")
-    print(groups.count().alcconsumption)
-    print("")
     
+    for group in groups:
+        #print("type of group: ", type(group),"len: ",len(group))
+        #print("types of tuple elements: <", type(group[0]), ", ",type(group[1]),">")
+        print(group[0],"\t",group[1]["alcconsumption"].count())
+    
+    #print(groups.count().alcconsumption)
+    print("")
+    """ 
     print("'incomperperson' is per capita income.")
     groups = get_groups(data, "incomeperperson")
     print(groups.count().incomeperperson)
@@ -51,6 +67,6 @@ if __name__ == "__main__":
     print("'lifeexpectancy' is life expectancy at birth, in years.")
     groups = get_groups(data, "lifeexpectancy")
     print(groups.count().lifeexpectancy)
-    print("")
-    
+    print("") 
+    """    
     print("done")
